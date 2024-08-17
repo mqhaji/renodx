ba510c81e4354f0d3bcf6982a208e0e1e329723c
Musa Haji <mqhaji@vt.edu>
2024-08-17 15:26:01 -0400

    maxpayne3: pull from refactor

---
+++ src/games/maxpayne3/shared.h
@@ -0,0 +1,25 @@
+#ifndef SRC_MAXPAYNE3_SHARED_H_
+#define SRC_MAXPAYNE3_SHARED_H_
+
+#ifndef __cplusplus
+#include "../../shaders/renodx.hlsl"
+#endif
+
+// Must be 32bit aligned
+// Should be 4x32
+struct ShaderInjectData {
+  float toneMapType;
+  float toneMapPeakNits;
+  float toneMapGameNits;
+  float toneMapUINits;
+  float toneMapHueCorrection;
+  float fxBloom;
+};
+
+#ifndef __cplusplus
+cbuffer cb13 : register(b13) {
+  ShaderInjectData injectedData : packoffset(c0);
+}
+#endif
+
+#endif  // SRC_MAXPAYNE3_SHARED_H_
